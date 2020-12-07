736b343bd90178bf7dbb67258244453b78af8c67
Lucas James Fostier <fostier.lucas@gmail.com>
2020-12-07 15:30:58 +0100

    Maj c++

--- D12 La traque/D12 La traque/Header.h
+++ D12 La traque/D12 La traque/Header.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
+#pragma warning (disable:4996)
+
+void FirstFillMap();
+void AffichMap();
---
+++ D12 La traque/D12 La traque/Main.cpp
@@ -0,0 +1,10 @@
+#include "Header.h"
+#include "Affichages.cpp"
+#include "Réflexions.cpp"
+
+void main()
+{
+    int Map[31][16] = { NULL };
+    FirstFillMap();
+    system("pause");
+}
